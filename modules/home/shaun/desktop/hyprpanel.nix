3aa6ca91018dc7495a773dc87009fc856327851a
spector700 <72362399+spector700@users.noreply.github.com>
2025-03-25 05:47:37 +0530

    Feat: Hyprpanel changes

--- modules/home/shaun/desktop/hyprpanel.nix
+++ modules/home/shaun/desktop/hyprpanel.nix
@@ -69,7 +69,7 @@ in
           ignored = "98";
         };
         windowtitle.label = false;
-        clock.format = "%a %d-%m-%Y  %k:%M:%S";
+        clock.format = "%a %d-%m-%Y  %H:%M:%S";
         clock.showIcon = false;
         volume.label = true;
         bluetooth.label = false;
@@ -77,115 +77,65 @@ in
         media.show_active_only = true;
       };
       menus.clock.weather.enable = false;
-      wallpaper.enable = false;
-    };
-    theme = {
-      # === Fonts ===
-      font.size = "1rem";
-
-      # === Bar Layout ===
-      bar = {
-        outer_spacing = "1rem";
-        dropdownGap = "3.3em";
-
-        background = background;
-        buttons = {
-          monochrome = true;
-          text = foreground;
-          radius = "${toString rounding}px";
-          background = background-alt;
-          icon = accent;
-          hover = background;
-
-          workspaces = {
-            hover = accent-alt;
-            active = accent;
-            available = accent-alt;
-          };
-
-          notifications = {
-            background = background-alt;
-            hover = background;
-            total = accent;
-            icon = accent;
-          };
-        };
-
-        menus = {
-          monochrome = true;
-
-          background = background;
-          cards = background-alt;
-          card_radius = "${toString rounding}px";
-          border = {
-            color = accent;
-            radius = "${toString rounding}px";
-          };
-          label = foreground;
-          text = foreground;
-
-          popover = {
-            text = foreground;
-            background = background-alt;
-          };
-
-          listitems.active = accent;
-          icons.active = accent;
-          switch.enabled = accent;
-
-          buttons = {
-            default = accent;
-            active = accent;
-          };
-
-          iconbuttons.active = accent;
-
-          progressbar.foreground = accent;
-          slider.primary = accent;
-
-          tooltip = {
-            background = background-alt;
-            text = foreground;
-          };
-
-          dropdownmenu = {
-            background = background-alt;
-            text = foreground;
-          };
-
-          menu.media = {
-            background.color = background-alt;
-            card.color = background-alt;
-            card.tint = 90;
-          };
-        };
-      };
-
-      # === Notifications ===
-      notification = {
-        background = background-alt;
-        border = background-alt;
-        border_radius = "${toString rounding}px";
-        label = accent;
-        labelicon = accent;
-        text = foreground;
-
-        actions = {
-          background = accent;
-          text = foreground;
-        };
-      };
-
-      # === OSD (On-screen Display) ===
-      osd = {
-        bar_color = accent;
-        bar_overflow_color = accent-alt;
-        icon = background;
-        icon_container = accent;
-        label = accent;
-        bar_container = background-alt;
+      wallpaper.enable = true;
+      theme = {
+        font.size = "0.9rem";
+        bar.outer_spacing = "1rem";
+        bar.dropdownGap = "3.3em";
+        bar.menus.monochrome = true;
+        bar.buttons.monochrome = true;
+        bar.buttons.workspaces.hover = "${accent-alt}";
+        bar.buttons.workspaces.active = "${accent}";
+        bar.buttons.workspaces.available = "${accent-alt}";
+        bar.menus.background = "${background}";
+        bar.menus.cards = "${background-alt}";
+        bar.menus.card_radius = "${toString rounding}px";
+        bar.menus.label = "${foreground}";
+        bar.menus.text = "${foreground}";
+        bar.menus.border.color = "${accent}";
+        bar.menus.border.radius = "${toString rounding}px";
+        bar.menus.popover.text = "${foreground}";
+        bar.menus.popover.background = "${background-alt}";
+        bar.menus.listitems.active = "${accent}";
+        bar.menus.icons.active = "${accent}";
+        bar.menus.switch.enabled = "${accent}";
+        bar.menus.buttons.default = "${accent}";
+        bar.menus.buttons.active = "${accent}";
+        notification.border_radius = "${toString rounding}px";
+        bar.menus.iconbuttons.active = "${accent}";
+        bar.menus.progressbar.foreground = "${accent}";
+        bar.menus.slider.primary = "${accent}";
+        bar.menus.tooltip.background = "${background-alt}";
+        bar.menus.tooltip.text = "${foreground}";
+        bar.menus.dropdownmenu.background = "${background-alt}";
+        bar.menus.dropdownmenu.text = "${foreground}";
+        bar.background = "${background}";
+        bar.buttons.text = "${foreground}";
+        bar.buttons.radius = "${toString rounding}px";
+        bar.buttons.background = "${background-alt}";
+        bar.buttons.icon = "${accent}";
+        bar.buttons.notifications.background = "${background-alt}";
+        bar.buttons.hover = "${background}";
+        bar.buttons.notifications.hover = "${background}";
+        bar.buttons.notifications.total = "${accent}";
+        bar.buttons.notifications.icon = "${accent}";
+        notification.background = "${background-alt}";
+        notification.actions.background = "${accent}";
+        notification.actions.text = "${foreground}";
+        notification.label = "${accent}";
+        notification.border = "${background-alt}";
+        notification.text = "${foreground}";
+        notification.labelicon = "${accent}";
+        osd.bar_color = "${accent}";
+        osd.bar_overflow_color = "${accent-alt}";
+        osd.icon = "${background}";
+        osd.icon_container = "${accent}";
+        osd.label = "${accent}";
+        osd.bar_container = "${background-alt}";
+        bar.menus.menu.media.background.color = "${background-alt}";
+        bar.menus.menu.media.card.color = "${background-alt}";
+        bar.menus.menu.media.card.tint = 90;
       };
     };
-
   };
 }
