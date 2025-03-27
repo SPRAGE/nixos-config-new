abc48f552887950b1e2dbd23b88cc52618ffbaca
SPRAGE <shauna.pai@gmail.com>
2025-03-28 05:28:22 +0530

    Feat: SWAY for shaunoffice

--- modules/home/shaunoffice/desktop/sway/config.nix
+++ modules/home/shaunoffice/desktop/sway/config.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  modifier = "Mod4"; # SUPER
+  mod = "Mod4"; # SUPER
   terminal = "${pkgs.kitty}/bin/kitty";
   fileManager = "${pkgs.xfce.thunar}/bin/thunar";
   launcher = "${pkgs.rofi}/bin/rofi -show drun";
@@ -17,7 +17,7 @@ in
     enable = true;
 
     config = {
-      inherit modifier;
+      modifier = mod;
       inherit terminal;
 
       startup = [
