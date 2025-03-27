dfadd56fbe10aaf59f86dd2f2294bbe658ee5610
SPRAGE <shauna.pai@gmail.com>
2025-03-27 13:10:48 +0530

    Feat: Server role

--- modules/home/pai/roles/server.nix
+++ modules/home/pai/roles/server.nix
@@ -10,6 +10,10 @@ in
 {
   config = mkIf cfg.enable {
     modules = {
+      # services.nextcloud-client.enable = mkDefault true;
+      # theme.stylix.enable = mkDefault true;
+      # programs.spicetify.enable = mkDefault true;
+      # programs.orca-slicer.enable = mkDefault true;
     };
   };
 }
