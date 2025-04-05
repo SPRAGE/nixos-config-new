6f4a0ee53487c6e670cd73864d4b026e2e3607a0
SPRAGE <shauna.pai@gmail.com>
2025-04-05 17:49:10 +0530

    Feat: SAmba share

--- modules/nixos/services/samba.nix
+++ modules/nixos/services/samba.nix
@@ -1,23 +1,37 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.services.sambaShares;
-in {
+in
+{
   options.services.sambaShares = {
     enable = lib.mkEnableOption "Samba with non-system users per share";
 
     shares = lib.mkOption {
       description = "List of Samba shares with access usernames.";
-      type = lib.types.listOf (lib.types.submodule {
-        options = {
-          name = lib.mkOption { type = lib.types.str; description = "Share name."; };
-          path = lib.mkOption { type = lib.types.path; description = "Path to directory."; };
-          allowedUsers = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
-            description = "List of Samba-only users allowed to access this share.";
+      type = lib.types.listOf (
+        lib.types.submodule {
+          options = {
+            name = lib.mkOption {
+              type = lib.types.str;
+              description = "Share name.";
+            };
+            path = lib.mkOption {
+              type = lib.types.path;
+              description = "Path to directory.";
+            };
+            allowedUsers = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              description = "List of Samba-only users allowed to access this share.";
+            };
           };
-        };
-      });
+        }
+      );
     };
   };
 
@@ -29,7 +43,7 @@ in {
       group = "samba-share";
     };
 
-    users.groups.samba-share = {};
+    users.groups.samba-share = { };
 
     environment.systemPackages = with pkgs; [ samba ];
 
@@ -38,41 +52,54 @@ in {
 
       settings = {
         global = {
-          server string = "nixos samba server";
+          "server string" = "nixos samba server";
           security = "user";
           "map to guest" = "never";
           "obey pam restrictions" = false;
         };
 
-        shares = lib.genAttrs (map (s: s.name) cfg.shares) (name:
+        shares = lib.genAttrs (map (s: s.name) cfg.shares) (
+          name:
           let
             share = lib.findFirst (s: s.name == name) cfg.shares;
-          in {
+          in
+          {
             path = share.path;
             writable = true;
             browseable = true;
-            valid users = share.allowedUsers;
+            "valid users" = share.allowedUsers;
             "force user" = "samba-share";
           }
         );
       };
     };
 
-    systemd.tmpfiles.rules = map (share:
-      "d ${share.path} 0755 samba-share samba-share"
-    ) cfg.shares;
+    systemd.tmpfiles.rules = map (share: "d ${share.path} 0755 samba-share samba-share") cfg.shares;
 
-    networking.firewall.allowedTCPPorts = [ 139 445 ];
-    networking.firewall.allowedUDPPorts = [ 137 138 ];
+    networking.firewall.allowedTCPPorts = [
+      139
+      445
+    ];
+    networking.firewall.allowedUDPPorts = [
+      137
+      138
+    ];
 
     # Optional activation script to create samba users
     system.activationScripts.sambaUsers = {
       text = ''
-        ${lib.concatStringsSep "\n" (lib.unique (lib.flatten (map (share:
-          map (user:
-            "echo '${user}:yourpassword' | ${pkgs.samba}/bin/smbpasswd -a -s ${user} || true"
-          ) share.allowedUsers
-        ) cfg.shares)))}
+        ${lib.concatStringsSep "\n" (
+          lib.unique (
+            lib.flatten (
+              map (
+                share:
+                map (
+                  user: "echo '${user}:yourpassword' | ${pkgs.samba}/bin/smbpasswd -a -s ${user} || true"
+                ) share.allowedUsers
+              ) cfg.shares
+            )
+          )
+        )}
       '';
     };
   };
