9db829587f5e49667ce0bb0ba21e8349e1ba13f4
SPRAGE <shauna.pai@gmail.com>
2025-04-05 17:38:30 +0530

    Feat: SAmba share

--- modules/nixos/services/samba.nix
+++ modules/nixos/services/samba.nix
@@ -1,22 +1,36 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.services.sambaShares;
-in {
+in
+{
   options.services.sambaShares = {
     enable = lib.mkEnableOption "Samba with non-system users per share";
     shares = lib.mkOption {
       description = "List of Samba shares with access usernames.";
-      type = lib.types.listOf (lib.types.submodule {
-        options = {
-          name = lib.mkOption { type = lib.types.str; description = "Share name."; };
-          path = lib.mkOption { type = lib.types.path; description = "Path to directory."; };
-          allowedUsers = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
-            description = "List of Samba-only users allowed to access this share.";
+      type = lib.types.listOf (
+        lib.types.submodule {
+          options = {
+            name = lib.mkOption {
+              type = lib.types.str;
+              description = "Share name.";
+            };
+            path = lib.mkOption {
+              type = lib.types.path;
+              description = "Path to directory.";
+            };
+            allowedUsers = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              description = "List of Samba-only users allowed to access this share.";
+            };
           };
-        };
-      });
+        }
+      );
     };
   };
 
@@ -35,34 +49,47 @@ in {
         guest ok = no
       '';
 
-      shares = lib.listToAttrs (map (share: {
-        name = share.name;
-        value = {
-          path = share.path;
-          writable = true;
-          browseable = true;
-          validUsers = share.allowedUsers;
-          force user = "samba-share";
-        };
-      }) cfg.shares);
+      shares = lib.listToAttrs (
+        map (share: {
+          name = share.name;
+          value = {
+            path = share.path;
+            writable = true;
+            browseable = true;
+            validUsers = share.allowedUsers;
+            "force user" = "samba-share";
+          };
+        }) cfg.shares
+      );
     };
 
-    systemd.tmpfiles.rules = map (share:
-      "d ${share.path} 0755 samba-share samba-share"
-    ) cfg.shares;
+    systemd.tmpfiles.rules = map (share: "d ${share.path} 0755 samba-share samba-share") cfg.shares;
 
-    networking.firewall.allowedTCPPorts = [ 139 445 ];
-    networking.firewall.allowedUDPPorts = [ 137 138 ];
+    networking.firewall.allowedTCPPorts = [
+      139
+      445
+    ];
+    networking.firewall.allowedUDPPorts = [
+      137
+      138
+    ];
 
     environment.systemPackages = with pkgs; [ samba ];
 
     system.activationScripts.sambaUsers = {
       text = ''
-        ${lib.concatStringsSep "\n" (lib.unique (lib.flatten (map (share:
-          map (user:
-            "echo '${user}:yourpassword' | ${pkgs.samba}/bin/smbpasswd -a -s ${user} || true"
-          ) share.allowedUsers
-        ) cfg.shares)))}
+        ${lib.concatStringsSep "\n" (
+          lib.unique (
+            lib.flatten (
+              map (
+                share:
+                map (
+                  user: "echo '${user}:yourpassword' | ${pkgs.samba}/bin/smbpasswd -a -s ${user} || true"
+                ) share.allowedUsers
+              ) cfg.shares
+            )
+          )
+        )}
       '';
     };
   };
