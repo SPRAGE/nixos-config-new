9040dcf782555f568cc91d71c171229222f250ac
SPRAGE <shauna.pai@gmail.com>
2025-04-06 15:53:30 +0530

    Refactor: Remove commented-out Samba configuration for clarity

--- hosts/dataserver/default.nix
+++ hosts/dataserver/default.nix
@@ -33,54 +33,7 @@ services.sambaAdvanced = {
     };
   };
 };
-  
-  # enable = true;
-#   securityType = "user";
-#   openFirewall = true;
-#   settings = {
-#     global = {
-#       "workgroup" = "WORKGROUP";
-#       "server string" = "smbnix";
-#       "netbios name" = "smbnix";
-#       "security" = "user";
-#       #"use sendfile" = "yes";
-#       #"max protocol" = "smb2";
-#       # note: localhost is the ipv6 localhost ::1
-#       "hosts allow" = "192.168.0. 127.0.0.1 localhost";
-#       "hosts deny" = "0.0.0.0/0";
-#       "guest account" = "nobody";
-#       "map to guest" = "bad user";
-#     };
-#     "public" = {
-#       "path" = "/mnt/shaun";
-#       "browseable" = "yes";
-#       "read only" = "no";
-#       "guest ok" = "no";
-#       "create mask" = "0644";
-#       "directory mask" = "0755";
-#       "force user" = "shaun";
-#       # "force group" = "groupname";
-#     };
-#     "private" = {
-#       "path" = "/mnt/karan";
-#       "browseable" = "yes";
-#       "read only" = "no";
-#       "guest ok" = "no";
-#       "create mask" = "0644";
-#       "directory mask" = "0755";
-#       "force user" = "karan";
-#       # "force group" = "groupname";
-#     };
-#   };
-# };
 
-# services.samba-wsdd = {
-#   enable = true;
-#   openFirewall = true;
-# };
-
-# networking.firewall.enable = true;
-# networking.firewall.allowPing = true;
 
   modules.hardware = {
 
