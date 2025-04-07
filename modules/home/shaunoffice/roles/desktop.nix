7400583e195790414a6a7b04f1565be597511066
SPRAGE <shauna.pai@gmail.com>
2025-04-07 08:07:49 +0530

    Fix: ShaunOffice: Remmina

--- modules/home/shaunoffice/roles/desktop.nix
+++ modules/home/shaunoffice/roles/desktop.nix
@@ -12,6 +12,11 @@ in
     modules = {
       # services.nextcloud-client.enable = mkDefault true;
       theme.stylix.enable = mkDefault true;
+      programs.remmina = {
+        disableToolbar = true;
+        disableTabbing = true;
+
+      };
       # programs.spicetify.enable = mkDefault true;
       # programs.orca-slicer.enable = mkDefault true;
     };
